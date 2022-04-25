163b68fd2a76c46806f5f0ae997a3b1232f4fc8d
Agata Szulc <agataszulc97@gmail.com>
2022-04-25 16:01:52 +0200

    showing completed and completing works

--- src/App.js
+++ src/App.js
@@ -15,19 +15,53 @@ function App() {
       completed: false,
     },
   ]);
+  const [showCompleted, setShowCompleted] = useState(true);
   function deleteItem(id) {
     setTasks((oldState) => oldState.filter((item) => item.id !== id));
   }
+  function submit(e) {
+    e.preventDefault();
+    const newTask = {
+      id: Math.random(),
+      task: e.target.elements.taskInput.value,
+      completed: false,
+    };
+    setTasks((oldState) => oldState.concat(newTask));
+  }
+
+  function toggleCompletion(id) {
+    setTasks((oldState) => {
+      return oldState.map((item) => {
+        if (id === item.id) {
+          const newItem = { ...item };
+          newItem.completed = !newItem.completed;
+          return newItem;
+        }
+        return item;
+      });
+    });
+  }
+  let all = tasks;
+  if (!showCompleted) {
+    all = tasks.filter((item) => item.completed === false);
+  }
   return (
     <div className="App">
-      <form>
+      <button
+        onClick={() => {
+          setShowCompleted((oldState) => !oldState);
+        }}
+      >
+        Show
+      </button>
+      <form onSubmit={submit}>
         <label htmlFor="taskInput">Add the task:</label>
-        <input type="text" id="tastInput"></input>
+        <input type="text" id="tastInput" name="taskInput"></input>
         <button>Add task</button>
       </form>
       <ul>
-        {tasks.map((task) => (
-          <Task deleteItem={deleteItem} {...task} />
+        {all.map((task) => (
+          <Task toggleCompletion={toggleCompletion} deleteItem={deleteItem} {...task} />
         ))}
       </ul>
     </div>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,9 +2,13 @@ export default function Task(props) {
   function handleDelete() {
     props.deleteItem(props.id);
   }
+  function handleCompletion() {
+    props.toggleCompletion(props.id);
+  }
   return (
     <li className={props.completed ? "completed" : ""}>
-      {props.task} <button onClick={handleDelete}>Remove </button>
+      {props.task} <button onClick={handleCompletion}>{props.completed ? "Undo" : "Complete"} </button>
+      <button onClick={handleDelete}>Remove </button>
     </li>
   );
 }
